0ab774c3816aae300771eb01395f12f28cbd5560
Sophie Shimasaki <Expertdoggo@outlook.com>
2025-06-30 21:56:32 -0700

    waow splitting!!!
    split split it can split ur input into 3! although poorly...

--- Program.cs
+++ Program.cs
@@ -12,7 +12,26 @@
                 Console.WriteLine("you can always type 'exit' to exit!! <333");
                 string? input = Console.ReadLine();
 
-                if (input.ToLower() == "exit") { Console.WriteLine("okie see you later cutie!!!! >~< ^^"); break; }
+                if (input.ToLower() == "exit")
+                { 
+                    Console.WriteLine("okie see you later cutie!!!! >~< ^^"); break;
+                }
+
+                try
+                {
+                    string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string part in parts) 
+                    {
+                        Console.WriteLine(part);
+                    }
+
+                    if (parts.Length != 3) 
+                    { 
+                        Console.WriteLine("oh! sorry could you enter an expression in the format of 'number operator number'? ty!!");
+                        continue;
+                    }
+                }
+                catch { }
 
             }
         }
